39162224bba5bfbc4dc13fa76af24fe660fab3bf
Wyrun <32933287+Wyrun@users.noreply.github.com>
2018-02-10 17:25:53 +0700

    Run

--- index.py
+++ index.py
@@ -1,3 +1,14 @@
+# (c) 2018 Max Shytov | Wyrun
+
+import random
+
+e = 2.71828182845904
+
+
+def sigmoid(x):
+    res = 1
+    res /= (1 + pow(e, -x))
+    return res
 
 class neuron:
     def __init__(self, weightAmount, ifGetSt):
@@ -7,12 +18,15 @@ class neuron:
         self.currentValue = float(0)
         self.previousValue = float(0)
         for i in range(self.weightAmount):
-            weights.append(float(0))
+            self.weights.append(float(0))
     def newValue(self, theValue):
-        self.previousValue = self.currentValue;
-        self.currentValue = theValue;
-        if ifGetSt:
+        self.previousValue = self.currentValue
+        self.currentValue = theValue
+        if self.ifGetSt:
             self.currentValue = sigmoid(self.currentValue)
+
+    def randMyWeights(self):
+        self.weights = list(map(lambda : random.randint(0,1000)/500 - 1, self.weights))
 
 class NN:
     def __init__(self, inputAmount, outputAmount, hiddenLayersAmount, neuronsOnEachLayer, ifInputsGetSt):
@@ -36,30 +50,40 @@ class NN:
             self.hidden[i].append(neuron(self.outputAmount, True))
 
     def run(self, inputs):
-        for i in self.inputAmount:
-            self.inputs.newValue(float(inputs[i]))
-        for i in self.neuronsOnEachLayer:
+        for i in range(self.inputAmount):
+            self.inputs[i].newValue(float(inputs[i]))
+        for i in range(self.neuronsOnEachLayer):
             newValue = float(0)
-            for j in self.inputAmount:
+            for j in range(self.inputAmount):
                 newValue += self.inputs[j].currentValue * self.inputs[j].weights[i]
             self.hidden[0][i].newValue(newValue)
-
-        for h in (self.hiddenLayersAmount - 1):
-            for i in self.neuronsOnEachLayer:
+        for h in range(self.hiddenLayersAmount - 1):
+            for i in range(self.neuronsOnEachLayer):
                 newValue = float(0)
-                for j in self.neuronsOnEachLayer:
+                for j in range(self.neuronsOnEachLayer):
                     newValue += self.hidden[h][j].currentValue * self.hidden[h][j].weights[i]
                 self.hidden[h + 1][i].newValue(newValue)
 
-        for i in self.outputAmount:
+        for i in range(self.outputAmount):
             newValue = float(0)
-            for j in self.neuronsOnEachLayer:
+            for j in range(self.neuronsOnEachLayer):
                 newValue += self.hidden[self.hiddenLayersAmount - 1][j].currentValue * self.hidden[self.hiddenLayersAmount - 1][j].weights[i]
             self.outputs[i].newValue(newValue)
+        res = []
+        for i in range(len(self.outputs)):
+            res.append(self.outputs[i].currentValue)
+        return res
 
+    def randomizeTheWeights(self):
+        for i in range(self.inputAmount):
+            self.inputs[i].randMyWeights
+        for i in range(self.hiddenLayersAmount):
+            for j in range(len(self.hidden[i])):
+                self.inputs[i].randMyWeights
+        return True
+    def Learn(self, DataX, DataY):
 
+MyNN = NN(1,1,1,1, False)
+MyNN.randomizeTheWeights()
 
-
-
-
-NN(1,1,1,1, False);
+print(MyNN.run([1]));
